30173576a3ebf435a06c1b2033fdc0e785254af5
Sander van Strijp <17788939+svanstrijp@users.noreply.github.com>
2024-11-10 23:49:43 +0100

    Update nc_listener.sh

--- nc_listener.sh
+++ nc_listener.sh
@@ -3,9 +3,8 @@ Description=Netcat Listener on Port 443
 After=network.target
 
 [Service]
-Type=simple
+Type=forking
 ExecStart=/usr/bin/screen -dmS nc_listener /usr/bin/nc -nlvp 443
-ExecStop=/usr/bin/killall -w -s SIGTERM screen
 Restart=always
 RestartSec=10
 
