09dae4981bad044d4af9830a2aec3f7c4344bae5
Sander van Strijp <17788939+svanstrijp@users.noreply.github.com>
2024-11-10 23:45:39 +0100

    Update nc_listener.sh

--- nc_listener.sh
+++ nc_listener.sh
@@ -5,7 +5,9 @@ After=network.target
 [Service]
 Type=simple
 ExecStart=/usr/bin/screen -dmS nc_listener /usr/bin/nc -nlvp 443
+ExecStop=/usr/bin/killall -w -s SIGTERM screen
 Restart=always
+RestartSec=10
 
 [Install]
 WantedBy=multi-user.target
