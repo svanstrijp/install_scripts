2774ac45e1e172f03858be728d33b8e0ccd79241
Sander van Strijp <17788939+svanstrijp@users.noreply.github.com>
2024-11-10 23:31:58 +0100

    Update nc_listener.sh

--- nc_listener.sh
+++ nc_listener.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-# Description: Netcat Listener on Port 443 in a screen session
-# Place this script in /etc/init.d/ to run on startup
+[Unit]
+Description=Netcat Listener on Port 443
+After=network.target
 
-# Start the listener in a detached screen session
-/usr/bin/screen -dmS nc_listener /usr/bin/nc -nlvp 443
+[Service]
+Type=simple
+ExecStart=/usr/bin/screen -dmS nc_listener /usr/bin/nc -nlvp 443
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
