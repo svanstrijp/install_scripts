4d183af5cd0577c1fc64bf873040914c2bc43ff7
Sander van Strijp <17788939+svanstrijp@users.noreply.github.com>
2024-11-15 17:58:47 +0100

    Update nc_listener.sh

--- nc_listener.sh
+++ nc_listener.sh
@@ -1,12 +1,17 @@
 [Unit]
-Description=Netcat Listener on Port 443
+Description=Interactive Persistent Netcat Listener
 After=network.target
 
 [Service]
-Type=forking
-ExecStart=/usr/bin/screen -dmS nc_listener /usr/bin/nc -nlvp 443
+ExecStart=/bin/nc -nlvp 443
 Restart=always
 RestartSec=10
+User=root
+StandardInput=tty
+StandardOutput=tty
+TTYPath=/dev/tty
+TTYReset=yes
+TTYVHangup=no
 
 [Install]
 WantedBy=multi-user.target
